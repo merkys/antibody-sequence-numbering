38f659c541a7f7d22801f7772777cb5c1388b67e
Wolchear <wolchear@Ubuntu.myguest.virtualbox.org>
2025-03-14 17:12:52 +0200

    Updated testing system

--- modules/Seq.pm
+++ modules/Seq.pm
@@ -5,126 +5,126 @@ use warnings;
 
 sub new
 {
-	my ($class, $seq_ref, $domain_ref) = @_;
-	my $self = {
-			seq 	=> $seq_ref-> {seq},
-			header 	=> $seq_ref-> {header},
-			id 	=> $seq_ref-> {id},
-			domain 	=> $domain_ref-> {domain},
-			organism => $domain_ref-> {organism},
-			score	=> $domain_ref-> {score},
-			bias	=> $domain_ref-> {bias},
-			FRs	=> [],
-			CDRs	=> [],
-			aligned_seq	=> '',
-			fixed_seq	=> '',
-			insertion_count	=> '',
-			imgt_numbering	=> ''
-			};
-	return bless $self, $class;
+    my ($class, $seq_ref, $domain_ref) = @_;
+    my $self = {
+                seq 	=> $seq_ref-> {seq},
+                header 	=> $seq_ref-> {header},
+                id 	=> $seq_ref-> {id},
+                domain 	=> $domain_ref-> {domain},
+                organism => $domain_ref-> {organism},
+                score	=> $domain_ref-> {score},
+                bias	=> $domain_ref-> {bias},
+                FRs	=> [],
+                CDRs	=> [],
+                aligned_seq	=> '',
+                fixed_seq	=> '',
+                insertion_count	=> '',
+                imgt_numbering	=> ''
+               };
+    return bless $self, $class;
 }
 
 
 sub getDomain
 {
-	my ($self) = @_;
-	return $self -> {domain}
+    my ($self) = @_;
+    return $self -> {domain}
 }
 
 sub getOrganism
 {
-	my ($self) = @_;
-	return $self -> {organism}
+    my ($self) = @_;
+    return $self -> {organism}
 }
 
 sub getSeq
 {
-	my ($self) = @_;
-	return $self -> {seq}
+    my ($self) = @_;
+    return $self -> {seq}
 }
 
 sub getId
 {
-	my ($self) = @_;
-	return $self -> {id}
+    my ($self) = @_;
+    return $self -> {id}
 }
 
 sub getHeader
 {
-	my ($self) = @_;
-	return $self -> {header}
+    my ($self) = @_;
+    return $self -> {header}
 }
 
 sub addFr
 {
-	my ($self, $fr) = @_;
-	push @{$self -> {FRs}}, $fr; 
+    my ($self, $fr) = @_;
+    push @{$self -> {FRs}}, $fr; 
 }
 
 sub getFr
 {
-	my ($self, $index) = @_;
-	return $self -> {FRs}[$index]
+    my ($self, $index) = @_;
+    return $self -> {FRs}[$index]
 }
 
 sub addCdr
 {
-	my ($self, $cdr) = @_;
-	push @{$self -> {CDRs}}, $cdr; 
+    my ($self, $cdr) = @_;
+    push @{$self -> {CDRs}}, $cdr; 
 }
 
 sub getCdr
 {
-	my ($self, $index) = @_;
-	return $self -> {CDRs}[$index]
+    my ($self, $index) = @_;
+    return $self -> {CDRs}[$index]
 }
 
 sub setAlignedSeq
 {
-	my ($self, $aligned_seq) = @_;
-	$self -> {aligned_seq} = $aligned_seq;
+    my ($self, $aligned_seq) = @_;
+    $self -> {aligned_seq} = $aligned_seq;
 }
 
 sub getAlignedSeq
 {
-	my ($self) = @_;
-	return $self -> {aligned_seq}
+    my ($self) = @_;
+    return $self -> {aligned_seq}
 }
 
 sub setFixedSeq
 {
-	my ($self, $fixed_seq) = @_;
-	$self -> {fixed_seq} = $fixed_seq;
+    my ($self, $fixed_seq) = @_;
+    $self -> {fixed_seq} = $fixed_seq;
 }
 
 sub getFixedSeq
 {
-	my ($self) = @_;
-	return $self -> {fixed_seq}
+    my ($self) = @_;
+    return $self -> {fixed_seq}
 }
 
 sub setInsertionCount
 {
-	my ($self, $insertion_count) = @_;
-	return $self -> {insertion_count} = $insertion_count;
+    my ($self, $insertion_count) = @_;
+    return $self -> {insertion_count} = $insertion_count;
 }
 
 sub getInsertionCount
 {
-	my ($self) = @_;
-	return $self -> {insertion_count}
+    my ($self) = @_;
+    return $self -> {insertion_count}
 }
 
 sub setImgtNumbering
 {
-	my ($self, $numbering_ref) = @_;
-	$self -> {imgt_numbering} = $numbering_ref;
+    my ($self, $numbering_ref) = @_;
+    $self -> {imgt_numbering} = $numbering_ref;
 }
 
 sub getImgtNumbering
 {
-	my ($self) = @_;
-	return $self -> {imgt_numbering}
+    my ($self) = @_;
+    return $self -> {imgt_numbering}
 }
 
 1;
--- tests/readme.txt
+++ tests/readme.txt
@@ -1,4 +1,4 @@
 to run testing:
 1) make generate_dirs
 2) make prepare_data
-3) test_homo | test_mus
+3) test_all
