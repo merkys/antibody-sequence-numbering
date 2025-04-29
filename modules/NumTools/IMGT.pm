1a7660aa333acd037c274a4ce13fbbc872b388a3
Wolchear <wolchear@Ubuntu.myguest.virtualbox.org>
2025-04-29 20:08:24 +0300

    Improve IMGT numbering: remove artificial gap in CDR3 caused by HMM alignment
    Previously, due to HMM profile alignment, a persistent gap was retained inside the CDR3 region, even when it should not have been present. This update fixes the issue by removing superfluous gaps from CDR3 when insertions are detected, resulting in a more accurate IMGT numbering.
    
    This change already led to a small improvement in Kabat conversion (+6 mus). It's possible that some other conversion mismatches were caused by incorrectly aligned sequences, and this should be further investigated.
    
    Additionally, the IMGT module has become quite large and monolithic after introducing new logic. Consider splitting it into separate files for alignment fixing, CDR correction, and numbering, as the current structure is becoming harder to navigate.

--- modules/NumTools/IMGT.pm
+++ modules/NumTools/IMGT.pm
@@ -8,13 +8,25 @@ use POSIX qw(ceil);
 our @EXPORT_OK = qw(numberSeq);
 our %EXPORT_TAGS = ( ALL => \@EXPORT_OK );
 
+use constant { FR1_END   => 26,
+               CDR1_END  => 38,
+               FR2_END   => 55,
+               CDR2_END  => 65,
+               FR3_END   => 104,
+               CDR3_END  => 117,
+               TOTAL_LEN => 128 };
+
+use constant { CDR1_MAX_LEN => 12,
+               CDR2_MAX_LEN => 10,
+               CDR3_MAX_LEN => 13 };
 
 sub numberSeq
 {
     my ($seq_obj, $error_fix_mode) = @_;
-    $seq_obj->setInsertionCount(checkInsertion($seq_obj->getAlignedSeq()));
-    $seq_obj->setFixedSeq(fixAlignment($seq_obj->getAlignedSeq(), $seq_obj->getInsertionCount(),
+    $seq_obj->setFixedSeq(fixAlignment($seq_obj->getAlignedSeq(),
                                        $seq_obj->getSeq(), $error_fix_mode));
+    my @fixed_array = split('', $seq_obj->getFixedSeq());
+    $seq_obj->setInsertionCount(checkInsertion(\@fixed_array));
     $seq_obj->setImgtNumbering(formNumbering($seq_obj->getFixedSeq(), $seq_obj->getInsertionCount()));
     
     return $seq_obj->getFixedSeq(), $seq_obj -> getImgtNumbering()
@@ -22,45 +34,74 @@ sub numberSeq
 
 sub checkInsertion
 {
-	my ($seq) = @_;
-	my $tillTheEnd_length =  length(join("", @{ $seq }[ 104 .. (scalar(@{$seq}) - 1) ]));
-	my $insertion_count = 0;
-	if($tillTheEnd_length > 24)
-	{
-		$insertion_count = $tillTheEnd_length - 24;
-	}
+    my ($seq) = @_;
+    my $tillTheEnd_length =  length(join("", @{ $seq }[ FR3_END .. (scalar(@{$seq}) - 1) ]));
+    my $insertion_count = 0;
+    if($tillTheEnd_length > 24)
+    {
+	    #print $tillTheEnd_length . "\n";
+        $insertion_count = $tillTheEnd_length - 24;
+        #print $insertion_count . "\n";
+    }
 	
-	return $insertion_count
+    return $insertion_count
 }
 
 sub fixAlignment
 {
-	my ($seq, $insertion_count, $original_seq, $error_fix_mode) = @_;
-	fixLowContextZones($seq, $original_seq) if $error_fix_mode eq 'fit';
-	my @seq = fixRareInsertions($seq);
-	my $cdr1 = fixCdr([@seq[26..37]], 12);
-	my $cdr2 = fixCdr([@seq[55..64]], 10);
-	my $tillTheEnd_length = length(join("", @seq[104..$#seq]));
-	my $cdr3;
-	if($insertion_count != 0)
-	{
-		$cdr3 = join("",@seq[104..116]);
-		
-	}
-	else
-	{
-		$cdr3 = fixCdr([@seq[104..116]], 13);
-	}
+    my ($seq, $original_seq, $error_fix_mode) = @_;
+    fixLowContextZones($seq, $original_seq) if $error_fix_mode eq 'fit';
+    my @seq = fixRareInsertions($seq);
+    @seq = fixCdr3Zone(\@seq);
+    my $cdr1 = fixCdr([@seq[FR1_END..CDR1_END - 1]], CDR1_MAX_LEN);
+    my $cdr2 = fixCdr([@seq[FR2_END..CDR2_END - 1]], CDR2_MAX_LEN);
+    my $tillTheEnd_length = length(join("", @seq[FR3_END..$#seq]));
+    my $cdr3 = checkInsertion([ @seq[FR3_END..$#seq] ])
+        ? join('', @seq[FR3_END..CDR3_END - 1])
+        : fixCdr([@seq[FR3_END..CDR3_END - 1]], CDR3_MAX_LEN);
 	
-    my $fixed_seq = join("", @seq[0..25]) 
-                    . $cdr1 
-                    . join("", @seq[38..54]) 
-                    . $cdr2 
-                    . join("", @seq[65..103]) 
-                    . $cdr3 
-                    . join("", @seq[117..$#seq]);
+    my $seq_str = join('', @seq);
+
+    my $fixed_seq = substr($seq_str, 0,        FR1_END)                 
+                  . $cdr1                                                      
+                  . substr($seq_str, CDR1_END, FR2_END - CDR1_END)       
+                  . $cdr2                                                      
+                  . substr($seq_str, CDR2_END, FR3_END - CDR2_END)        
+                  . $cdr3
+                  . substr($seq_str, CDR3_END);
+    #print length($fixed_seq) . "\n";
     return $fixed_seq
 }
+
+
+sub fixCdr3Zone
+{
+    my ($seq_ref) = @_;
+
+    my @cdr3 = @{ $seq_ref }[ FR3_END .. CDR3_END - 1 ];
+    my $aa_count = grep { $_ ne '-' } @cdr3;
+
+    return @{ $seq_ref } if $aa_count == CDR3_MAX_LEN;
+
+    my $aligned_len = @{ $seq_ref };
+    my $fr4_len     = TOTAL_LEN - CDR3_END;               
+    my $tail_len    = $aligned_len - (CDR3_END);     
+    my $ins_to_fix  = $tail_len - $fr4_len;
+       $ins_to_fix  = 0 if $ins_to_fix < 0;
+
+    
+    for ( my $i = CDR3_END - 1; $i >= FR3_END && $ins_to_fix > 0; $i-- )
+    {
+        if ( $seq_ref->[$i] eq '-' )
+        {
+            splice( @{ $seq_ref }, $i, 1 );
+            $ins_to_fix--;
+        }
+    }
+
+    return @{ $seq_ref };
+}
+
 
 sub fixLowContextZones
 {
@@ -130,7 +171,9 @@ sub fixCdr
     
     my @no_gap_seq = grep { $_ ne '-' } @{ $cdr_ref };
     my $cdr_current_length = scalar(@no_gap_seq);
-    
+    die "CDR length ($cdr_current_length) exceeds max ($cdr_max_length)"
+        if $cdr_current_length > $cdr_max_length;
+        
     return "-" x $cdr_max_length if $cdr_current_length == 0;
     return join("", @no_gap_seq) if $cdr_max_length == $cdr_current_length;
 	
