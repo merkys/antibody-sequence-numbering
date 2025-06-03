373af09146a36ee885f901f2c2a53c21279cbc78
Andrius Merkys <andrius.merkys@gmail.com>
2025-06-03 10:58:13 +0300

    Minor syntax changes.

--- modules/Structures/HMM_model.pm
+++ modules/Structures/HMM_model.pm
@@ -8,7 +8,7 @@ use List::Util qw(reduce);
 
 sub new
 {
-    my ($class, $seq_ref) = @_;
+    my( $class, $seq_ref ) = @_;
     
     my $tmp_fasta = writeFastaToTMP($seq_ref->{id}, $seq_ref->{seq});
     my @hmm_scan_out = runHmmScan($tmp_fasta);
@@ -29,45 +29,45 @@ sub new
                            score	=> $seq->{score},
                            bias	    => $seq->{bias}};
         
-        push @seq_objects, Structures::Seq->new($sub_seq_ref, $domain_ref);
+        push @seq_objects, Structures::Seq->new( $sub_seq_ref, $domain_ref );
     }
     
-    my $self = { original_seq => $seq_ref->{seq},
-                 header       => $seq_ref->{header},
-                 id           => $seq_ref->{id},
-                 sequences    => \@seq_objects};
+    my $self = bless { original_seq => $seq_ref->{seq},
+                       header       => $seq_ref->{header},
+                       id           => $seq_ref->{id},
+                       sequences    => \@seq_objects },
+                     $class;
                  
-    for my $seq (@{ $self->{sequences} })
-    {
-        $seq->setAlignedSeq(alignToDomain($seq->getId(), $seq->getSeq(),
-                                      $seq->getDomain(), $seq->getOrganism()));
+    for my $seq ($self->getSequences) {
+        $seq->setAlignedSeq(alignToDomain($seq->getId, $seq->getSeq,
+                                          $seq->getDomain, $seq->getOrganism));
     }
-    return bless $self, $class;
+    return $self;
 }
 
 
 sub getSequences
 {
-    my ($self) = @_;
+    my( $self ) = @_;
     return @{$self->{sequences}};
 }
 
 sub writeFastaToTMP
 {
-    my ($header, $seq_ref) = @_;
-    my ($tmp_fh, $tmp_fasta_file) = tempfile( UNLINK => 1 );
+    my( $header, $seq_ref ) = @_;
+    my( $tmp_fh, $tmp_fasta_file ) = tempfile( UNLINK => 1 );
     print $tmp_fh ">" . $header . "\n";
-    print $tmp_fh join('', @$seq_ref), "\n";
-    close($tmp_fh);
+    print $tmp_fh join( '', @$seq_ref ), "\n";
+    close $tmp_fh;
     return $tmp_fasta_file;
 }
 
 sub runHmmScan
 {
-    my ($tmp_fasta) = @_;
+    my( $tmp_fasta ) = @_;
     
-    my ($fh, $dom_tblout) = tempfile( UNLINK => 1 );
-    close($fh);
+    my( $fh, $dom_tblout ) = tempfile( UNLINK => 1 );
+    close $fh;
     
     my $params = "--domT 80 --domtblout $dom_tblout --noali";
     my $hmm_file = 'hmms/IG_combined.hmm';
@@ -122,8 +122,7 @@ sub detectSequences
 
     @hits = sort { $a->{start} <=> $b->{start} } @hits;
     my @segments;
-    for my $hit (@hits)
-    {
+    for my $hit (@hits) {
         my ($f, $t) = ($hit->{start}, $hit->{end}); # ali_from, ali_to
         if (!@segments || $f > $segments[-1]{end})
         {
@@ -132,14 +131,13 @@ sub detectSequences
         else
         {
             $segments[-1]{end} = $t if $t > $segments[-1]{end};
-            push @{ $segments[-1]{hits} }, $hit;
+            push @{$segments[-1]{hits}}, $hit;
         }
     }
     
     my @sequences;
-    for my $seg (@segments)
-    {
-        my @h = @{ $seg->{hits} };
+    for my $seg (@segments) {
+        my @h = @{$seg->{hits}};
         
         if( my( $heavy ) = grep { $_->{domain} eq 'IGH' } @h ) {
             push @sequences, { domain => 'IGH',
@@ -174,7 +172,7 @@ sub alignToDomain
     
     local $/ = "\n";
     my @hmm_align_results = qx($command);
-    chomp(@hmm_align_results);
+    chomp @hmm_align_results;
     my $result_str = join("", @hmm_align_results[1 .. $#hmm_align_results]);
     my @residues = split //, $result_str;
     return \@residues;
